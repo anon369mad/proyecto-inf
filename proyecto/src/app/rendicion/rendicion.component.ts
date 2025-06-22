7fe86616e0fa5af59a9d469161caa9cc9a76ab80
vibacach <vibacach@alumnos.inf.utfsm.cl>
2025-06-22 19:09:18 -0400

    Se abortan cambios.

--- proyecto/src/app/rendicion/rendicion.component.ts
+++ proyecto/src/app/rendicion/rendicion.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@
 import { Rendicion } from '../rendicion.model';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-//import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { Observable } from 'rxjs';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+
 
 @Component({
   selector: 'app-rendicion',
@@ -11,43 +13,41 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './rendicion.component.html',
   styleUrl: './rendicion.component.css'
 })
+
 export class RendicionComponent {
 
-  APIURL = "http://localhost:8000/rendiciones";
-
-  constructor(private http: HttpClient) {}
+  APIURL = "http://localhost:8000/rendiciones"
+  constructor(private http: HttpClient){}
 
   @Input() rendicion: Rendicion | null = null;
+
   @Output() cerrar = new EventEmitter<void>();
   @ViewChild('rendicionModal') rendicionModal!: ElementRef;
 
-  documentos: { id: number, url: string }[] = [];
-
   openByDocument() {
-    if (this.rendicion?.id) {
+    if (this.rendicion && this.rendicion.id) {
       console.log('Abriendo modal para rendición:', this.rendicion.id);
       this.obtenerDocumentos(this.rendicion.id);
     }
     if (this.rendicionModal) {
       this.rendicionModal.nativeElement.classList.remove('hidden');
-      this.rendicionModal.nativeElement.classList.add('flex'); // Mostrar modal
+      this.rendicionModal.nativeElement.classList.add('flex') // Mostrar modal
     }
   }
-
   closeRendicionByDocument() {
     if (this.rendicionModal) {
       this.rendicionModal.nativeElement.classList.add('hidden');
-      this.rendicionModal.nativeElement.classList.remove('flex'); // Ocultar modal
+      this.rendicionModal.nativeElement.classList.remove('flex') // Ocultar modal
     }
   }
 
-  rechazarRendicion(rendicionId: number): void {
+  rechazarRendicion(rendicionId: number): void{
     if (rendicionId == null) {
       console.error('El ID de la rendición no es válido');
       return;
     }
     const url = `${this.APIURL}/${rendicionId}/rechazar`;
-    const body = { estado: 'Rechazada' };
+    const body = {estado: 'Rechazada'};
     this.http.post(url, body).subscribe(
       (response) => {
         console.log('Rendición rechazada:', response);
@@ -55,18 +55,18 @@ export class RendicionComponent {
         window.location.reload();
       },
       (error) => {
-        console.error('Error al rechazar la rendición:', error);
+        console.error('Error al rechazar la rendicion:', error);
       }
     );
   }
 
-  aceptarRendicion(rendicionId: number): void {
+  aceptarRendicion(rendicionId: number): void{
     if (rendicionId == null) {
       console.error('El ID de la rendición no es válido');
       return;
     }
     const url = `${this.APIURL}/${rendicionId}/aceptar`;
-    const body = { estado: 'Aceptada' };
+    const body = {estado: 'Aceptada'};
     this.http.post(url, body).subscribe(
       (response) => {
         console.log('Rendición aceptada:', response);
@@ -74,10 +74,12 @@ export class RendicionComponent {
         window.location.reload();
       },
       (error) => {
-        console.error('Error al aceptar la rendición:', error);
+        console.error('Error al aceptar la rendicion:', error);
       }
     );
   }
+
+  documentos: { id: number, url: string }[] = [];
 
   obtenerDocumentos(rendicionId: number): void {
     const url = `${this.APIURL}/${rendicionId}/documentos`;
